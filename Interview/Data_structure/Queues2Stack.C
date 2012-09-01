d99819751d99563b3c4b8519c79aff9f6f1fcf99
wuhong <xunzhangthu@gmail.com>
2012-09-01 20:59:57 +0800

    Queues2Stack.C added

---
+++ Interview/Data_structure/Queues2Stack.C
@@ -0,0 +1,71 @@
+/* Queues2Stack.C */
+#include <queue>
+#include <iostream>
+
+template<typename T> class QueStack {
+
+public:
+  
+  QueStack() {}
+  
+  virtual ~QueStack() {}
+  
+  int empty() {
+    return q1.empty() && q2.empty();
+  }
+  
+  void push(const T& val) {
+    if(empty()) 
+      q1.push(val);
+    else if (q2.empty()) {
+      q2.push(val);
+      T tmp;
+      while(!q1.empty()) {
+        tmp = q1.front();
+        q2.push(tmp);
+        q1.pop();
+      }
+    } else {
+      q1.push(val);
+      T tmp;
+      while(!q2.empty()) {
+        tmp = q2.front();
+        q1.push(tmp);
+        q2.pop();
+      }
+    }
+  }
+  
+  void pop() {
+    if(empty()) 
+      return;
+    else if(q1.empty()) 
+      q2.pop();
+    else 
+      q1.pop();
+  }
+  
+  const T& top() {
+    if(q1.empty()) 
+      return q2.back();
+    else 
+      return q1.back();
+  }
+ 
+private:
+  std::queue<int> q1;
+  std::queue<int> q2;
+};
+
+
+int main()
+{
+  QueStack<int> stk;
+  stk.push(1);  
+  stk.push(2);  
+  stk.push(3);  
+  stk.pop();
+  std::cout << stk.top() << std::endl;
+  stk.push(4);  
+  stk.push(5);  
+}
